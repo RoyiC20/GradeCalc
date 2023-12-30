8bca5234b87da065bce5c3631edf80c7bbef05b0
RoyiC <royi.carmel@gmail.com>
2023-12-30 22:52:08 +0200

    adding game class and sounds

--- GradeCalc/Game.cs
+++ GradeCalc/Game.cs
@@ -6,76 +6,38 @@ using System.Threading.Tasks;
 
 namespace GradeCalc
 {
-    internal class Game
-    {
-    }
 
-    public class game
+    public class Game
     {
-        private string name = ""; // name of answerer
-        private string grade = ""; // grade of the answerer
         private int n1 = 0; // first number
         private int n2 = 0; // second number 
         private int type = 0; //decides on the type of the exercise (+, -, *, /)
-        private int counter = 1; // counts the questions
-        private int expected = 0; // the answer that is expected
-        private int correctCounter = 0; // the number of correct answers
+        int expected = 0; // the answer that is expected
 
 
 
-
-        private string getName()
-        {
-            return this.name;
-        }
-
-        private string getGrade()
-        {
-            return this.grade;
-        }
-
-        private int getN1()
-        {
-            return this.n1;
-        }
-        private int getN2()
-        {
-            return this.n2;
-        }
-        private int getType()
-        {
-            return this.type;
-        }
-        private int getCounter()
-        {
-            return this.counter;
-        }
-        private int getExpected()
+        public int getExpected()
         {
             return this.expected;
         }
-        private int getCorrectCounter()
-        {
-            return this.correctCounter;
-        }
 
-        private void modifyNumbers() // chooses the numbers randomly
+        public void modifyNumbers(string grade) // chooses the numbers randomly
         {
 
             Random rnd = new Random();
-            if (this.grade == "Grade A")
+            if (grade == "Grade A")
             {
                 // Grade A: Addition and Subtraction with numbers up to 20
                 this.n1 = rnd.Next(0, 21);
                 this.n2 = rnd.Next(0, 21);
             }
-            else if (this.grade == "Grade B")
+            else if (grade == "Grade B")
             {
                 // Grade B: Addition and Subtraction with numbers up to 100
                 this.n1 = rnd.Next(0, 101);
                 this.n2 = rnd.Next(0, 101);
             }
-            else if (this.grade == "Grade C")
+            else if (grade == "Grade C")
             {
                 // Grade C: Addition and Subtraction with numbers up to 1000 
                 // Division with numbers up to 100 (the left side of division) and up to 10 (the right side), Multiplication up to 10
@@ -94,7 +56,7 @@ namespace GradeCalc
                     this.n2 = rnd.Next(1, 11); // this is 1 because there cant be division by 0
                 }
             }
-            else if (this.grade == "Grade D")
+            else if (grade == "Grade D")
             {
                 // Grade D: Addition and Subtraction with numbers up to 10000
                 // Division with numbers up to 2500 (the left side of division) and up to 50 (the right side), Multiplication up to 50
@@ -114,6 +76,72 @@ namespace GradeCalc
                 }
             }
         }
+
+        public string signDecider() // decides on the sign of the question (+, -, *, /)
+        {
+            // + == 0
+            // - == 1
+
+            // * == 0
+            // / == 1
+
+
+            Random rnd = new Random();
+            int num = rnd.Next(0, 2);
+
+            if (this.type == 0)
+            {
+                if (num == 0)
+                    return "+";
+                else
+                    return "-";
+            }
+            else
+            {
+                if (num == 0)
+                    return "*";
+                else
+                    return "/";
+            }
+        }
+
+        public string questionMaker() // makes the question in string form and finds the answer in int
+        {
+            int n1 = this.n1;
+            int n2 = this.n2;
+
+            string currentSign = signDecider();
+            if (currentSign == "+") // makes it a + exercise
+            {
+                this.expected = this.n1 + this.n2;
+            }
+            else if (currentSign == "-") // makes it a - exercise and puts the bigger number first
+            {
+                if (n1 > n2)
+                {
+                    this.expected = this.n1 - this.n2;
+                }
+                else
+                {
+                    this.expected = this.n2 - this.n1;
+                    n2 = this.n1;
+                    n1 = this.n2;
+                }
+            }
+            else if (currentSign == "*") // makes it a * exercise
+            {
+                this.expected = this.n1 * this.n2;
+            }
+            else  // makes it a / exercise by doing (N1 * N2) / N2 to guarentee full numbers
+            {
+                this.expected = (this.n1 * this.n2) / this.n2;
+                n1 = this.n1 * this.n2;
+            }
+
+
+            return ($"{n1} {currentSign} {n2}"); // returns the question in string
+        }
+
 
 
 
--- GradeCalc/MainWindow.xaml.cs
+++ GradeCalc/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,12 +24,11 @@ namespace GradeCalc
     {
         string name = ""; // name of answerer
         string grade = ""; // grade of the answerer
-        int n1; // first number
-        int n2; // second number 
-        int type = 0; //decides on the type of the exercise (+, -, *, /)
         int counter = 1; // counts the questions
-        int expected = 0; // the answer that is expected
         int correctCounter = 0; // the number of correct answers
+
+        Game game = new Game();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -67,139 +68,13 @@ namespace GradeCalc
 
         private void prepareNextQuestion() //makes the next question
         {
-            modifyNumbers();
+            game.modifyNumbers(this.grade);
             answerTextBox.Text = "";
-            question.Text = questionMaker();
+            question.Text = game.questionMaker();
             questionNumber.Text = ($"Question number {this.counter}");
             this.counter++;
         }
 
-        
-        private void modifyNumbers() // chooses the numbers randomly
-        {
-
-            Random rnd = new Random();
-            if (this.grade == "Grade A")
-            {
-                // Grade A: Addition and Subtraction with numbers up to 20
-                this.n1 = rnd.Next(0, 21);
-                this.n2 = rnd.Next(0, 21);
-            }
-            else if (this.grade == "Grade B")
-            {
-                // Grade B: Addition and Subtraction with numbers up to 100
-                this.n1 = rnd.Next(0, 101);
-                this.n2 = rnd.Next(0, 101);
-            }
-            else if (this.grade == "Grade C")
-            {
-                // Grade C: Addition and Subtraction with numbers up to 1000 
-                // Division with numbers up to 100 (the left side of division) and up to 10 (the right side), Multiplication up to 10
-
-                Random rnd2 = new Random(); // makes it random between being a devision or multi, or being a addition or substraction
-                this.type = rnd2.Next(0, 2);
-
-                if (this.type == 0)
-                {
-                    this.n1 = rnd.Next(0, 1001);
-                    this.n2 = rnd.Next(0, 1001);
-                }
-                else
-                {
-                    this.n1 = rnd.Next(0, 11);
-                    this.n2 = rnd.Next(1, 11); // this is 1 because there cant be division by 0
-                }
-            }
-            else if (this.grade == "Grade D")
-            {
-                // Grade D: Addition and Subtraction with numbers up to 10000
-                // Division with numbers up to 2500 (the left side of division) and up to 50 (the right side), Multiplication up to 50
-
-                Random rnd2 = new Random(); // makes it random between being a devision or multi, or being a addition or substraction
-                this.type = rnd2.Next(0, 2);
-
-                if (this.type == 0)
-                {
-                    this.n1 = rnd.Next(0, 10001);
-                    this.n2 = rnd.Next(0, 10001);
-                }
-                else
-                {
-                    this.n1 = rnd.Next(0, 51);
-                    this.n2 = rnd.Next(1, 51); // this is 1 because there cant be division by 0
-                }
-            }
-        }
-
-
-
-
-        private string signDecider() // decides on the sign of the question (+, -, *, /)
-        {
-            // + == 0
-            // - == 1
-
-            // * == 0
-            // / == 1
-
-
-            Random rnd = new Random();
-            int num = rnd.Next(0, 2);
-
-            if (this.type == 0)
-            {
-                if (num == 0)
-                    return "+";
-                else 
-                    return "-";
-            }
-            else
-            {
-                if (num == 0)
-                    return "*";
-                else
-                    return "/";
-            }
-        }
-
-
-
-        private string questionMaker() // makes the question in string form and finds the answer in int
-        {
-            int n1 = this.n1;
-            int n2 = this.n2;
-
-            string currentSign = signDecider();
-            if (currentSign == "+") // makes it a + exercise
-            {
-                this.expected = this.n1 + this.n2;
-            }
-            else if (currentSign == "-") // makes it a - exercise and puts the bigger number first
-            {
-                if (n1 > n2)
-                {
-                    this.expected = this.n1 - this.n2;
-                }
-                else
-                {
-                    this.expected = this.n2 - this.n1;
-                    n2 = this.n1;
-                    n1 = this.n2;
-                }
-            }
-            else if (currentSign == "*") // makes it a * exercise
-            {
-                this.expected = this.n1 * this.n2;
-            }
-            else  // makes it a / exercise by doing (N1 * N2) / N2 to guarentee full numbers
-            {
-                this.expected = (this.n1 * this.n2) / this.n2;
-                n1 = this.n1 * this.n2;
-            }
-
-
-            return ($"{n1} {currentSign} {n2}"); // returns the question in string
-        }
 
 
         private void switchVisibility() // if the first panel is visible, it'll make the second panel visible and turn itself to collapsed and keeps going to the third panel
@@ -220,10 +95,27 @@ namespace GradeCalc
         private void nextQuestion_Click(object sender, RoutedEventArgs e)
         {
 
-            if (answerTextBox.Text == this.expected.ToString()) //checks if the answer is right
+
+            if (answerTextBox.Text == game.getExpected().ToString()) //checks if the answer is right
             {
+                
+
                 this.correctCounter++;
+
+
+                SoundPlayer playsound = new SoundPlayer("Sounds/success.wav"); // plays success sound
+                playsound.Load();
+                playsound.Play();
+
             }
+            else
+            {
+                SoundPlayer playsound = new SoundPlayer("Sounds/error.wav");  // plays error sound
+                playsound.Load();
+                playsound.Play();
+            }
+
+
             if (this.counter == 10) // if the counter reaches 10 it changes thee text on the button
             {
                 nextQuestion.Content = "Finish test and get results";
@@ -237,6 +129,8 @@ namespace GradeCalc
 
                 return;
             }
+
+
 
             prepareNextQuestion();
         }
